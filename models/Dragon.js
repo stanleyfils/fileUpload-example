f1ca8a2629587e52492605171d4a29b400935b2e
Stanley Fils <stanleyfils@gmail.com>
2020-02-13 23:37:08 -0500

    added model

---
+++ models/Dragon.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const {
+  Schema,
+  model
+} = mongoose;
+
+const dragonSchema = new Schema({
+  name: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  kewlFactor: {
+    type: Number,
+    enum: [1, 2, 3, 4, 5]
+  },
+  image: {
+    type: String
+  }
+})
+
+const Dragon = model('Dragon', dragonSchema);
+module.exports = Dragon;
